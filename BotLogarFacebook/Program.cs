aa58e420d50d71a4a0ccdab7bacc764d7e0ce537
Mateus Donaire <canalmateusdonaire@gmail.com>
2021-07-08 02:36:10 -0300

    Bot para logar no Facebook - Selenium

---
+++ BotLogarFacebook/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Threading;
+
+namespace BotLogarFacebook
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://www.facebook.com/";
+            string email = "your@email.com";
+            string pass = "yourpassword";
+
+            FirefoxDriver driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl(url);
+            driver.Manage().Window.Maximize();
+
+            Thread.Sleep(1000);
+
+            var emailEl = driver.FindElementById("email");
+            emailEl.SendKeys(email);
+
+            var passEl = driver.FindElementById("pass");
+            passEl.SendKeys(pass);
+
+            var loginEl = driver.FindElementByName("login");
+            loginEl.Click();
+
+            Thread.Sleep(3000);
+
+            var profileEl = driver.FindElementByXPath("//span[@class='a8c37x1j ni8dbmo4 stjgntxs l9j0dhe7']");
+            profileEl.Click();
+
+            Thread.Sleep(5000);
+            driver.Close();
+        }
+    }
+}
